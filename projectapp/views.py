5c7c690a3faaaa3b2686d0281ae5d9d965f3571e
EchoGhos <alwinjose357@gmail.com>
2025-01-11 13:09:58 +0530

    Updated alwin staff

--- projectapp/views.py
+++ projectapp/views.py
@@ -72,7 +72,68 @@ def station_profile(request):
         form = station_profile_form(instance = details)
         form2 = station_email_form(instance=login_details)
     return render(request,'edit_station.html',{'form' : form,'form2' : form2})
-    
+
+def staffreg(request):
+    if request.method=='POST':
+        form=staffform(request.POST)
+        logins=loginform(request.POST)
+        if form.is_valid() and logins.is_valid():
+            a=logins.save(commit=False)
+            a.usertype=1
+            a.save()
+            b=form.save(commit=False)
+            b.staff_login_id = a
+            b.save()
+            return redirect('home')
+    else:
+         form=staffform()
+         logins=loginform()
+    return render(request,'staff_reg.html',{'forms':form,'log':logins})
+
+
+def staff_home(request):
+    return render(request,'staff_home.html')
+
+def login_page(request):
+    if request.method=='POST':
+        form= login_check(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            try:
+                user = login.objects.get(email=email)
+                if user.password == password:
+                    if user.usertype == 1:  
+                        request.session['userid']=user.id
+                        return redirect('staffhome')
+                else:
+                    messages.error(request,'Invalid Password')
+            except login.DoesNotExist:
+                messages.error(request,'user does not exist')
+    else:
+        form=login_check()            
+    return render(request,'login.html',{'form': form})
+
+
+
+
+
+def staff_profile(request):
+    loginid = request.session.get('userid')
+    login_details = get_object_or_404(login,id=loginid)
+    details = get_object_or_404(staff_reg,staff_login_id= login_details)
+    if request.method == 'POST':
+        form = staff_profile_form(request.POST, instance=details)
+        forms = staff_email_form(request.POST,instance=login_details)
+        if form.is_valid() and forms.is_valid():
+            form.save()
+            forms.save()
+            return redirect('staffhome')
+    else:
+        form = staff_profile_form(instance = details)
+        forms = staff_email_form(instance = login_details)
+    return render(request , 'edit_staff.html',{'form': form,'forms' : forms})
+
 
 
 
