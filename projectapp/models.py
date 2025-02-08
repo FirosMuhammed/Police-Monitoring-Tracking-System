01747b5032a166bd6312a85a2b94ce78851780fb
FirosMuhammed <firosmuhammed010@gmail.com>
2025-02-08 16:51:51 +0530

    duty

--- projectapp/models.py
+++ projectapp/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 
 class police_station(models.Model):
@@ -11,6 +12,8 @@ class police_station(models.Model):
     contact = models.CharField(max_length=15)
     login_id = models.ForeignKey('login',on_delete=models.CASCADE)
     
+    def __str__(self):
+        return self.addressline1
 
 
 class login(models.Model):
@@ -25,6 +28,7 @@ class user_reg(models.Model):
     login_userid=models.ForeignKey('login',on_delete=models.CASCADE,blank=True,null=True)
 
 class staff_reg(models.Model):
+    id=models.AutoField(primary_key=True)
     staff_id=models.CharField(max_length=100)
     staff_name=models.CharField(max_length=100)
     staff_address=models.CharField(max_length=100)
@@ -33,6 +37,7 @@ class staff_reg(models.Model):
     staff_contact=models.CharField(max_length=100)
     staff_dob=models.CharField(max_length=100)
     staff_designation=models.CharField(max_length=100)
+    staff_station= models.ForeignKey('police_station',on_delete=models.CASCADE,blank=True,null=True)
 
     GENDER_CHOICES = [
         ('Male', 'Male'),
@@ -58,4 +63,14 @@ class criminals(models.Model):
     gender = models.CharField(max_length=10, choices=GENDER_CHOICES, default='Male')
 
     login_staffid=models.ForeignKey('staff_reg',on_delete=models.CASCADE,blank=True,null=True)
+
+
+
+
+class duties(models.Model):
+    duty = models.CharField(max_length=100)
+    current_date= models.DateField(auto_now_add=True)
+    staff_login_id = models.ForeignKey(staff_reg,on_delete=models.CASCADE)
+    login_id=models.ForeignKey(login,on_delete=models.CASCADE,default=0)
+    
    
